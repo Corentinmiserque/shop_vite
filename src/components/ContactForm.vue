22684c7fc8f9d444c627e52cf00d1e3efdfbc583
Corentinmiserque <corentinmiserque1@gmail.com>
2024-01-11 10:44:50 +0100

    first commit

---
+++ src/components/ContactForm.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h1>Contact</h1>
+    
+    <form @submit.prevent="submitForm">
+      <label for="name">Nom:</label>
+      <input type="text" v-model="newContact.name" id="name" >
+      
+      <label for="email">Email:</label>
+      <input type="email" id="email"v-model="newContact.email" >
+      
+      
+      <button type="submit">Envoyer</button>
+    </form>
+  </div>
+</template>
+
+<script setup>
+//import
+import { ref } from 'vue';
+
+
+//code
+const newContact = ref({
+  name: "",
+  email:""
+})
+const emit = defineEmits(['soumettre'])
+
+const submitForm = () => {
+  alert('test');
+  emit('soumettre'), newContact.value
+};
+
+</script>
---
+++ src/views/ContactView.vue
@@ -0,0 +1,13 @@
+
+<template>
+  <contactForm @soumettre="newContact"/>
+
+</template>
+
+<script setup>
+import ContactForm from '@/components/ContactForm.vue';
+
+const newContact = (infos) => {
+  console.log(infos.name)
+}
+</script>
